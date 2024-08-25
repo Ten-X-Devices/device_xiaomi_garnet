23e1ce0256fe9ab5be5ec08a8bc1feb655ea5370
JYRRC <javixhtsweet@gmail.com>
2024-08-25 14:32:38 +0200

    garnet: bringUp tenxOS
    Signed-off-by: JYRRC <javixhtsweet@gmail.com>

--- lineage_garnet.mk
+++ lineage_garnet.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CrDroid stuff.
+# Inherit some common TenX stuff.
 TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
@@ -18,7 +18,7 @@ $(call inherit-product, device/xiaomi/garnet/device.mk)
 # MiuiCamera
 $(call inherit-product-if-exists, vendor/xiaomi/garnet-miuicamera/products/miuicamera.mk)
 
-# CrDroid stuff
+# TenX stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_HAS_UDFPS := true
 EXTRA_UDFPS_ANIMATIONS := true
