1eb72a5d1383803249cfb972d47f7d0d742e0f5e
JYRRC <javixhtsweet@gmail.com>
2024-08-25 14:52:30 +0200

    Revert "garnet: parts: Drop doze related features"
    This reverts commit 77355a705f99b5ae22e27c030e541d2dbb26443f.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -27,6 +27,7 @@ import android.util.Log;
 import android.view.Display;
 import android.view.Display.HdrCapabilities;
 
+import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 
@@ -36,12 +37,13 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-
         if (!intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
             return;
         }
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
+	// Doze
+        DozeUtils.onBootCompleted(context);
 
         // Thermal Profiles
         ThermalUtils.startService(context);
